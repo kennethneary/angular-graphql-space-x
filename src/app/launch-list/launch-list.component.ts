17bd963e206395be7c8c19780ad26c2537cb822b
Kenneth Neary <kennethneary@gmail.com>
2020-06-13 15:11:52 +0100

    add components and generated graphql code

---
+++ src/app/launch-list/launch-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { PastLaunchesListGQL } from '../services/spacexGraphql.service';
+
+@Component({
+  selector: 'app-launch-list',
+  templateUrl: './launch-list.component.html',
+  styleUrls: ['./launch-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class LaunchListComponent implements OnInit {
+
+  constructor(private readonly pastLaunchesService: PastLaunchesListGQL) {}
+
+  pastLaunches$ = this.pastLaunchesService
+    // Please be care to not fetch too much, but this amount lets us see the img lazy loading in action
+    .fetch({ limit: 30 })
+    .pipe(
+      map((res) => res.data.launchesPast)
+    );
+
+    ngOnInit(): void {
+    }
+}
